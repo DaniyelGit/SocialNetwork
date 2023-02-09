d0dede946a16f8a20ec48291ad41d24c10c48010
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-02-09 22:52:36 +0300

    added initial skeleton (html/css) of the social network

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+function App() {
+   return (
+      <div className="app-wrapper">
+         <header className={'header'}>
+            <img src="#" alt="logo"/>
+         </header>
+         <nav className={'nav'}>
+            <ul>
+               <li>
+                  <a href={'#'}>Profile</a>
+               </li>
+               <li>
+                  <a href={'#'}>Messages</a>
+               </li>
+               <li>
+                  <a href={'#'}>News</a>
+               </li>
+               <li>
+                  <a href={'#'}>Music</a>
+               </li>
+               <li>
+                  <a href={'#'}>Settings</a>
+               </li>
+            </ul>
+         </nav>
+         <div className={'content'}>
+            <div>
+               <img src="#" alt="bg-content"/>
+            </div>
+            <div>
+               ava + description
+            </div>
+            <div>
+               My post
+               <div>
+                  New post
+                  <div>
+                     <div>
+                        post 1
+                     </div>
+                     <div>
+                        post 2
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default App;
