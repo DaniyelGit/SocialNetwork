6253fae9e1bd772235c7f3c9d14ac7435e97e2e1
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-02-18 14:52:06 +0300

    added module.css and style for Dialogs.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -7,8 +7,36 @@ type DialogsPropsType = {
 
 export const Dialogs = (props: DialogsPropsType) => {
    return (
-      <div>
-         Dialogs
+      <div className={s.dialogs}>
+        <div className={s.dialogsItem}>
+           <div className={`${s.dialog} ${s.active}`}>
+               Даниель
+           </div>
+           <div className={s.dialog}>
+              Виктория
+           </div>
+           <div className={s.dialog}>
+              Евгений
+           </div>
+           <div className={s.dialog}>
+              Артур
+           </div>
+           <div className={s.dialog}>
+              Захар
+           </div>
+        </div>
+
+         <div className={s.messages}>
+            <div className={s.message}>
+               Hi
+            </div>
+            <div className={s.message}>
+               How is your it-kamasutra
+            </div>
+            <div className={s.message}>
+               Yo
+            </div>
+         </div>
       </div>
    );
 };
