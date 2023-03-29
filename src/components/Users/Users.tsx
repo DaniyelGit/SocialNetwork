3273f7b6ffe8e23dff33f4848f2091b19b283c29
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-03-29 22:49:08 +0300

    removed the side effect from the component Users.tsx (wrapped the side effect with a declared function)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,23 +4,26 @@ import axios from "axios";
 
 export const Users = (props: UsersPropsType) => {
 
-   if (props.usersState.length === 0) {
-      axios.get('https://social-network.samuraijs.com/api/1.0/users')
-         .then(response => {
-            console.log(response)
-            props.setUsers(response.data.items);
-         })
-   };
+   const getUsers = () => {
+      if (props.usersState.length === 0) {
+         axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+               console.log(response)
+               props.setUsers(response.data.items);
+            })
+      };
+   }
 
    return (
       <div>
+         <button onClick={getUsers}>get users</button>
          {
             props.usersState.map(u => {
                return (
                   <div key={u.id + '-user'}>
                      <div>
                         <div>
-                           <img src={u.photos.small !== null ? u.photos.small : 'https://via.placeholder.com/100x100'} alt="avatar-user"/>
+                           <img src={u.photos.small? u.photos.small : 'https://via.placeholder.com/100x100'} alt="avatar-user"/>
                         </div>
                         <div>
                            {
