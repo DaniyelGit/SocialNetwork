737291fe8e65c33fd76b5161849222403e97680f
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-04-08 12:31:01 +0300

    added component Preloader.tsx

---
+++ src/common/Preloader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Preloader = () => {
+   return (
+      <div>
+         <img src="../images/svg-loaders/preloader.svg" alt="preloader"/>
+      </div>
+   );
+};
