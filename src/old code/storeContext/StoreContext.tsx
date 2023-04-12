01310c7e73a6750db9af7050735ac4ade054846f
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-04-12 21:58:45 +0300

    test commit

---
+++ src/old code/storeContext/StoreContext.tsx
@@ -0,0 +1,18 @@
+import {createContext, ReactNode} from "react";
+import {StoreType} from "../../redux/redux-store/redux-store";
+
+export const StoreContext = createContext({} as StoreType);
+
+// !!! type for Provider !!!
+type ProviderType = {
+   store: StoreType
+   children: ReactNode
+}
+
+export const Provider = (props: ProviderType) => {
+   return (
+      <StoreContext.Provider value={props.store}>
+         {props.children}
+      </StoreContext.Provider>
+   );
+}
