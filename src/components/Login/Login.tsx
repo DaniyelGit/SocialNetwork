1bdc9240479d0941979ceafb9100369432ef775c
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-05-15 01:24:26 +0300

    added redux-form to the project, also added functionality and typing

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,33 +1,51 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
 type LoginPropsType = {}
 
+// тип для других пропсов если понадобится
+interface IProps {
+
+}
+
+type FormDataType = {
+   login: string;
+   password: string;
+   rememberMe: boolean;
+}
+
 export const Login = (props: LoginPropsType) => {
+   const onSubmit = (formData: FormDataType) => {
+      console.log(formData)
+   }
+
    return (
       <div>
          <h2>LOGIN</h2>
-         <LoginForm/>
+         <LoginReduxForm onSubmit={onSubmit}/>
       </div>
    );
 };
 
-export const LoginForm: React.FC = (props) => {
-
-
+export const LoginForm: React.FC<InjectedFormProps<FormDataType, IProps>> = (props) => {
    return (
-      <form>
+      <form onSubmit={props.handleSubmit}>
          <div>
-            <input type="text" name='login' placeholder='Login'/>
+            <Field type='text' name='login' placeholder='Login' component='input'/>
          </div>
          <div>
-            <input type="text" name='password' placeholder='Password'/>
+            <Field type='text' name='password' placeholder='Password' component='input'/>
          </div>
          <div>
-            <input type="checkbox" name="remember"/> remember me
+            <Field type='checkbox' name='rememberMe' component='input'/>
          </div>
          <div>
             <button>Log in</button>
          </div>
       </form>
    );
-}
+};
+
+const LoginReduxForm = reduxForm<FormDataType, IProps>({
+   form: 'login'
+})(LoginForm);
